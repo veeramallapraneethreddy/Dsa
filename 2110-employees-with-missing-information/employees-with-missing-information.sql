c5ea72c0c16b9c4233e594815bd5764742f74377
Praneeth_reddy <2303a52146@sru.edu.in>
2026-09-05 14:31:07 +0530

    Time: 544 ms (90.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select e.employee_id from employees e 
+left join salaries s  on e.employee_id=s.employee_id where salary is null
+union
+select s.employee_id from employees e 
+right join salaries s  on e.employee_id=s.employee_id 
+where name is null order by employee_id;
